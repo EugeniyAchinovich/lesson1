abac66c5e706b4dcc4579dd54ae3bef0bdae4d11
Eugene Achinovich <e.achinovitch@yandex.ru>
2023-08-26 16:09:53 +0300

    Added sum function

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,12 @@
 package org.example;
 
 public class Main {
+    public static int sum(int a, int b) {
+        return a + b;
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int a = 5, b = 2;
+
+        System.out.println(sum(a, b));
     }
 }
